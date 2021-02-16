c9aede66ef95faafa9916a2b4e2a2e6fd48c0b5e
Jeison Crespo <jeisoncrespo15@gmail.com>
2021-02-16 10:17:57 -0400

    arreglo de un fallo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { KeyPipe } from './core/pipes/key.pipe';
 import { TypeDefencesPipe } from './core/pipes/type-defences.pipe';
 import { KeysPipe } from './core/pipes/keys.pipe';
 
 @NgModule({
-  declarations: [AppComponent, KeyPipe, TypeDefencesPipe, KeysPipe],
+  declarations: [AppComponent, TypeDefencesPipe, KeysPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
